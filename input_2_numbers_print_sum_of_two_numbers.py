fb84d7d8d8ec5e390bb34285c2935deeb7c77ae3
jadensanares <sanaresj@gmail.com>
2025-03-12 01:15:53 +0800

    following the coding standards for batch 1 codes

--- input_2_numbers_print_sum_of_two_numbers.py
+++ input_2_numbers_print_sum_of_two_numbers.py
@@ -1,4 +1,8 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
 # ask the user to input 2 numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
 # print the result of the sum of the two numbers
+print("The sum of the two numbers is", number1 + number2)
