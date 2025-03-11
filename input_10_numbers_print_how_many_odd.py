55e7cd7c310723346c094908e0d597475ff1e46b
jadensanares <sanaresj@gmail.com>
2025-03-12 01:39:51 +0800

    finished the code by following the coding standard

--- input_10_numbers_print_how_many_odd.py
+++ input_10_numbers_print_how_many_odd.py
@@ -1,4 +1,13 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
 # define odd number
+odd_number = 0
+
 # make the equation
+for i in range (1, 11):
+    number = int(input("Enter a number: "))
+    if number % 2 != 0:
+        odd_number += 1
+
 # print the result
+print("Total:", odd_number)
