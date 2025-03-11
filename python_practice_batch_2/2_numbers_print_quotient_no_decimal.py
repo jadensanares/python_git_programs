aed0fd1f0c0805a1b183e5127fda29c003927097
jadensanares <sanaresj@gmail.com>
2025-03-12 07:50:48 +0800

    pseudocode added

---
+++ python_practice_batch_2/2_numbers_print_quotient_no_decimal.py
@@ -0,0 +1,3 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+# ask the user to input 2 numbers
+# make the conditions and print the result
