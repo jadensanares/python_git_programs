8bf8ef10399b6c768723279129d67329b7410644
jadensanares <sanaresj@gmail.com>
2025-03-12 07:42:39 +0800

    pseudocode added

---
+++ python_practice_batch_2/2_numbers_print_not_equal_if_not_same_.py
@@ -0,0 +1,3 @@
+# Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+# Ask the user to input the first and second number
+# Compare both of the numbers if they are equal
