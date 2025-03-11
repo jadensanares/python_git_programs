1b40883a2227201f65715793e3d96031b15afbb4
jadensanares <sanaresj@gmail.com>
2025-03-12 07:52:23 +0800

    finished the code by following the coding standard

--- python_practice_batch_2/2_numbers_print_quotient_no_decimal.py
+++ python_practice_batch_2/2_numbers_print_quotient_no_decimal.py
@@ -1,3 +1,13 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
 # ask the user to input 2 numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
 # make the conditions and print the result
+if number2 == 0:
+    print("Error: Cannot divide by zero.")
+
+else:
+    quotient = number1 // number2
+    print("The quotient of the two numbers is", quotient)
