ba85ed661d02a73fb5a816a56feb415ff59c0346
jadensanares <sanaresj@gmail.com>
2025-03-12 01:23:32 +0800

    pseudocode added

---
+++ input_2_numbers_print_quotient_with_decimal_point.py
@@ -0,0 +1,3 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+# ask the user to input 2 numbers
+# make the conditions for finding the quotient
