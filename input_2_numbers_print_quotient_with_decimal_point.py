9922eee506c131d85d9e4e739116269c05fa71a7
jadensanares <sanaresj@gmail.com>
2025-03-12 01:25:41 +0800

    finished the code by following the coding standard

--- input_2_numbers_print_quotient_with_decimal_point.py
+++ input_2_numbers_print_quotient_with_decimal_point.py
@@ -1,3 +1,13 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
 # ask the user to input 2 numbers
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
 # make the conditions for finding the quotient
+if number2 == 0:
+    print("Undefined")
+
+else:
+    quotient = number1 / number2
+    print("The quotient is", quotient)
