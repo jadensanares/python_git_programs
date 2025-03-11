b9e5b766c59b768ab153e822e5d466a2e63c4fa7
jadensanares <sanaresj@gmail.com>
2025-03-12 07:59:49 +0800

    pseudocode added

---
+++ python_practice_batch_2/input_10_numbers_print_firstnumber_minus_remaining.py
@@ -0,0 +1,5 @@
+# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+# Start a list to store the numbers
+# Ask the user to input 10 numbers
+# Calculate the result of the first number minus all of the remaining numbers
+# Subtract each of the remaining numbers from the result
