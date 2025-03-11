b076414fcb3767a7c0a56af9172f67df78987935
jadensanares <sanaresj@gmail.com>
2025-03-12 00:47:57 +0800

    added pseudocode

---
+++ input_2_numbers_print_bigger_number.py
@@ -0,0 +1,5 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+# tell the user to input 2 numbers
+# make an if/else statement on determining the bigger number
+# print the result
