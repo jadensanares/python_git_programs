c11a2c131b114b83277f2bfcfbc4fda6baff2f22
jadensanares <sanaresj@gmail.com>
2025-03-12 07:48:11 +0800

    finished the code by following the coding standard

--- python_practice_batch_2/2_numbers_print_difference.py
+++ python_practice_batch_2/2_numbers_print_difference.py
@@ -1,3 +1,10 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
 # ask the user to input 2 numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second numver: "))
+
 # make the equation and print the result
+difference = number1 - number2
+
+print("The difference is:", difference)
