c3269968a6b2eae21cdeab37947a7fc1b49f4508
jadensanares <sanaresj@gmail.com>
2025-03-12 01:29:47 +0800

    finished the code by following the coding standard

--- input_2_numbers_first_number_raised_to_second_print_result.py
+++ input_2_numbers_first_number_raised_to_second_print_result.py
@@ -1,3 +1,8 @@
 # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
 # ask the user the input 2 numbers
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
 # make the equation for raising a number and print the result
+print(number1 ** number2)
