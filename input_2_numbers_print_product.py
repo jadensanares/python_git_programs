dd65d8a51a7c8edfcc5964bcd7129000a9df338e
jadensanares <sanaresj@gmail.com>
2025-03-12 01:19:11 +0800

    pseudocode created

---
+++ input_2_numbers_print_product.py
@@ -0,0 +1,3 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+# ask the user to input 2 numbers
+# make the equation to find the product and print the result
