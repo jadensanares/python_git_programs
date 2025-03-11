b3ffc67affde17b345b20e32a7015e1c87685b50
jadensanares <sanaresj@gmail.com>
2025-03-12 07:44:04 +0800

    finished the code by following the coding standard

--- python_practice_batch_2/2_numbers_print_not_equal_if_not_same_.py
+++ python_practice_batch_2/2_numbers_print_not_equal_if_not_same_.py
@@ -1,3 +1,11 @@
 # Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
 # Ask the user to input the first and second number
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
 # Compare both of the numbers if they are equal
+if number1 != number2:
+    print("Not Equal")
+else:
+    print("Both numbers are equal.")
