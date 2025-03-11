323627c7c12c1bcd99d10e7b10fd00c5f4f4b250
jadensanares <sanaresj@gmail.com>
2025-03-12 02:24:35 +0800

    finished the code by following the coding standards

--- python_practice_batch_2/2_numbers_print_smaller.py
+++ python_practice_batch_2/2_numbers_print_smaller.py
@@ -1,2 +1,11 @@
 # Create a program that ask user to input 2 numbers. Print the smaller number.
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
 # Compare the two numbers and print the bigger one
+if number1 > number2:
+    print("The smaller number is:", number2)
+elif number2 > number1:
+    print("The smaller number is:", number1)
+else:
+    print("Both of the numbers are equally the same.")
