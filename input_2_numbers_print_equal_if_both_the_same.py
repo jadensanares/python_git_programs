89303fa0f043e4e55ab0935423660fba8d514e5e
jadensanares <sanaresj@gmail.com>
2025-03-12 01:03:40 +0800

    added the user input and the if/else statement, finishing the code

--- input_2_numbers_print_equal_if_both_the_same.py
+++ input_2_numbers_print_equal_if_both_the_same.py
@@ -1,4 +1,12 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
 # tell the user to input 2 numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
 # calculate if both numbers are equal, if it is print equal
+if number1 == number2:
+    print("Both numbers are equal")
 # use else statement if the result is not equal
+else:
+    print("Both numbers are not equal")
