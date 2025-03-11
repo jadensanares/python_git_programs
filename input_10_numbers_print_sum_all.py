6a4d4a58c40a423e4e7641f918a5ad0bd7da9b92
jadensanares <sanaresj@gmail.com>
2025-03-12 01:34:45 +0800

    finished the code by following the coding standard

--- input_10_numbers_print_sum_all.py
+++ input_10_numbers_print_sum_all.py
@@ -1,4 +1,13 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
 # define the sum
+sum = 0
+
+
 # make the conditions
+for i in range(1, 11):
+    number = int(input("Enter a number: "))
+    sum += number
+
 # print the result
+print("The total sum is", sum)
