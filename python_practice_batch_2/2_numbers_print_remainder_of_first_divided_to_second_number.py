709b68c002d60a9ae1bccb84028c5b7018f135e2
jadensanares <sanaresj@gmail.com>
2025-03-12 07:56:32 +0800

    finished the code by following the coding standard

--- python_practice_batch_2/2_numbers_print_remainder_of_first_divided_to_second_number.py
+++ python_practice_batch_2/2_numbers_print_remainder_of_first_divided_to_second_number.py
@@ -1,3 +1,13 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
 # Ask the user to input 2 numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
 # Create the conditions then print the result
+if number2 == 0:
+    print("Cannot divide by zero.")
+
+else:
+    remainder = number1 % number2
+    print("The remainder is", remainder)
