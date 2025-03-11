86400661ad7093e3932083f4c248916d44389aec
jadensanares <sanaresj@gmail.com>
2025-03-12 00:53:20 +0800

    ask user to input 2 numbers

--- input_2_numbers_print_bigger_number.py
+++ input_2_numbers_print_bigger_number.py
@@ -1,5 +1,8 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
 # tell the user to input 2 numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
 # make an if/else statement on determining the bigger number
 # print the result
