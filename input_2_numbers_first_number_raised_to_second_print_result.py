0f54d104aa352f0f30a26e0501814d0053574e1f
jadensanares <sanaresj@gmail.com>
2025-03-12 01:28:33 +0800

    pseudocode added

---
+++ input_2_numbers_first_number_raised_to_second_print_result.py
@@ -0,0 +1,3 @@
+# Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+# ask the user the input 2 numbers
+# make the equation for raising a number and print the result
