b0f3b4ae5c7645ae502dcc682b88420eba0b8de9
jadensanares <sanaresj@gmail.com>
2025-03-12 01:47:34 +0800

    finished the code by following the coding standard

--- print_all_numbers_0_to_100_except_ending_with_zero.py
+++ print_all_numbers_0_to_100_except_ending_with_zero.py
@@ -1,2 +1,6 @@
 # Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
 # set the conditions and print the result
+for number in range (101):
+    if number % 10 != 0:
+        print (number)
