c7b5016c487d32adec5526001dd500ca6b7bba6d
jadensanares <sanaresj@gmail.com>
2025-03-12 01:43:23 +0800

    finished the code by following the coding standard

--- print_all_even_numbers_0_to_100.py
+++ print_all_even_numbers_0_to_100.py
@@ -1,2 +1,6 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
 # set the conditions and then print the reuslt
+for number in range (101):
+    if number % 2 == 0:
+        print(number)
