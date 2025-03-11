261dc1f24d7499a1c4ec6a71a8c96e0cee3f09ba
jadensanares <sanaresj@gmail.com>
2025-03-12 01:20:32 +0800

    finished the code following the coding standard

--- input_2_numbers_print_product.py
+++ input_2_numbers_print_product.py
@@ -1,3 +1,8 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
 # ask the user to input 2 numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
 # make the equation to find the product and print the result
+print("The product of the two numbers is", number1 * number2)
