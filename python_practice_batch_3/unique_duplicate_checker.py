04ece393f0862d592a585097dcae59a7db1bcb9e
jadensanares <sanaresj@gmail.com>
2025-03-12 09:51:19 +0800

    tesing if the break works if the user prompts to exit the program

--- python_practice_batch_3/unique_duplicate_checker.py
+++ python_practice_batch_3/unique_duplicate_checker.py
@@ -1,5 +1,12 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+
 # Initialize emty set to keep track of seen numbers
-# Prompt the user to input a number
-# Check for invalid input (letters)
-# Check if the number has been seen before
+seen_numbers = set()
+
+while True:
+    user_input = input("Enter a number (or type 'quit' to quit): ")
+
+    if user_input.lower() == 'quit':
+        print("Quitting the program.")
+        break
+    
