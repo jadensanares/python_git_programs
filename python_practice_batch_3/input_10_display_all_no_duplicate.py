f4ce8cc295f7b4e5ba61db7f4c3ae7bd632fb03e
jadensanares <sanaresj@gmail.com>
2025-03-12 08:56:41 +0800

    testing of the user input works

--- python_practice_batch_3/input_10_display_all_no_duplicate.py
+++ python_practice_batch_3/input_10_display_all_no_duplicate.py
@@ -1,4 +1,19 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
-# Initialize Variables
+
+# Initialize an empty list in able to store numbers
+number_list = []
+# Initialize an empty dictionary to count occurences of each number
+number_count = {}
+
 # Prompt user to input 10 numbers
+for i in range(10):
+    number = int(input(f"Enter the number {i + 1}: ")) # To get the user input
+    number_list.append(number) # Adding number to list
+
+    # Count the occurences of the number
+    if number in number_count:
+        number_count[number] += 1
+    else:
+        number_count[number] = 1
+
 # Diplay the numbers that do not have duplicates
