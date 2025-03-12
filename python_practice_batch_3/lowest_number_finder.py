b6bc5136d4a9289b349ccfbdff43e1a30e6905f7
jadensanares <sanaresj@gmail.com>
2025-03-12 10:09:50 +0800

    pseudocode added

---
+++ python_practice_batch_3/lowest_number_finder.py
@@ -0,0 +1,8 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+# Initialize a variable to keep track on the lowest number
+# Make a loop to continue asking for input
+# Prompt the user to enter a number
+# Creating an option for the user to quit the program
+# Converting input to integer
+# update the lowest number if there are changes with the valid input 
+# after the loop, display the lowest number
