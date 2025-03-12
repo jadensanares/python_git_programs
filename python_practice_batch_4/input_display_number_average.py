25ace21648f97f1e620de6986264424dde49d66f
jadensanares <sanaresj@gmail.com>
2025-03-12 22:35:27 +0800

    added the prompt for the user

--- python_practice_batch_4/input_display_number_average.py
+++ python_practice_batch_4/input_display_number_average.py
@@ -1,4 +1,18 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
 # define the variables
+total_sum = 0
+count = 0
+
 # prompt user for input
+while True:
+    user_input = input("Input a number: ")
+
+    try:
+        number = float(user_input)
+        total_sum += number
+        count += 1
+    except ValueError:
+        break
+
 # display the average if the numbers entered were valid
