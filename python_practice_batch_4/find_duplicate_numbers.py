9e0262a4f81e602d2fed0391fb34c8414eb76a6d
jadensanares <sanaresj@gmail.com>
2025-03-12 13:38:54 +0800

    pseudocode added

---
+++ python_practice_batch_4/find_duplicate_numbers.py
@@ -0,0 +1,7 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+# Initialize empty list to store numbers
+# Prompt the user to input 10 numbers
+# Make a set to store duplicates
+# Make a set to track see numbers
+# look for duplicates
+# display results
