9ae2613d01f27f5346a8c2cf5249252366698b81
jadensanares <sanaresj@gmail.com>
2025-03-12 08:14:00 +0800

    pseudocode added

---
+++ python_practice_batch_2/input_10_print_even_numbers.py
@@ -0,0 +1,5 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+# Start the counter for even numbers
+# Ask the user to input 10 numbers 
+# Check if the number is even
+
