a229eb2a77601e690c6b9f2c6cca7dbab324b814
jadensanares <sanaresj@gmail.com>
2025-03-12 09:07:56 +0800

    pseudocode added

---
+++ python_practice_batch_3/input_10_display_all_duplicated_numbers_display_only_firstentry.py
@@ -0,0 +1,4 @@
+# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+# Initialize the variables
+# Prompt the user input
+# Display the numbers showing duplicates only once
