96ed57a24cfd9624a86497b3b025c8e87d2d845d
jadensanares <sanaresj@gmail.com>
2025-03-12 08:15:58 +0800

    finished the code by following the coding standard

--- python_practice_batch_2/input_10_print_even_numbers.py
+++ python_practice_batch_2/input_10_print_even_numbers.py
@@ -1,5 +1,14 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
-# Start the counter for even numbers
-# Ask the user to input 10 numbers 
-# Check if the number is even
 
+# Start the counter for even numbers
+even_count = 0
+
+# Ask the user to input 10 numbers 
+for i in range(10):
+    number = int(input(f"Enter number {i + 1}: "))
+
+# Check if the number is even
+    if number % 2 == 0:
+            even_count += 1 
+
+print("The total number of even numbers is:", even_count)
