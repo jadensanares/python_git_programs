103e9c7ec1e9dc51787c0115fb13bbd9eb6a9acd
jadensanares <sanaresj@gmail.com>
2025-03-12 22:15:04 +0800

    added the while true, for valid number to list, infinit loop if non-integer

--- python_practice_batch_4/sort_numbers_lowest_highest.py
+++ python_practice_batch_4/sort_numbers_lowest_highest.py
@@ -1,2 +1,13 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
 # ask the user continously until input is invalid. Display the number from highest to lowest
+numbers = []
+
+while True:
+    user_input = input("Input a number: ")
+
+    try: 
+        nubmers = int(user_input)
+        numbers.append(number)
+    except ValueError:
+        continue
