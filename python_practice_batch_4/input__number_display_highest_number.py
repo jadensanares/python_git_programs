ee8ba56528e60c5a0b7d57647afb2d130c98f790
jadensanares <sanaresj@gmail.com>
2025-03-12 17:21:52 +0800

    added a print statement for instructions to the user, program code finished for Prog03 Batch 4

--- python_practice_batch_4/input__number_display_highest_number.py
+++ python_practice_batch_4/input__number_display_highest_number.py
@@ -1,6 +1,7 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
 # ask the user continously until input is invalid. Display highest number
+print("Input a number until it is invalid to display the highest number.")
 highest_number = None
 
 while True:
