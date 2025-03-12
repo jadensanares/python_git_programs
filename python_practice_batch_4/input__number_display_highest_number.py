e34da1e4a12700dab1886b09970ed0c5cd779711
jadensanares <sanaresj@gmail.com>
2025-03-12 17:17:17 +0800

    checking if the if statement works and breaks if something invalid 'no text was typed' ends the program

--- python_practice_batch_4/input__number_display_highest_number.py
+++ python_practice_batch_4/input__number_display_highest_number.py
@@ -1,3 +1,11 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
-# ask the user continously until input is invalid. Display highest number
 
+# ask the user continously until input is invalid. Display highest number
+highest_number = None
+
+while True:
+    user_input = input("Input a number:" )
+
+    if user_input.strip() == "": # This is to check if the input is empty
+        print("Highest Number:", highest_number)
+        break
