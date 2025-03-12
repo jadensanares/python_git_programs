4c15a3e5238df99991a9a3eee147bb1871ca4601
jadensanares <sanaresj@gmail.com>
2025-03-12 13:45:10 +0800

    testing code and is having errors

--- python_practice_batch_4/find_duplicate_numbers.py
+++ python_practice_batch_4/find_duplicate_numbers.py
@@ -1,7 +1,23 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
 # Initialize empty list to store numbers
+numbers = []
+
 # Prompt the user to input 10 numbers
+for i in range(10):
+    while True:
+        try:
+            # ask the user for a number
+            num = int(input(f"Enter number {i + 1}: "))
+            numbers.appendd(num)
+            break 
+        except ValueError:
+            print("Input invalid, Enter a valid integer.")
+
 # Make a set to store duplicates
+
 # Make a set to track see numbers
+
 # look for duplicates
+
 # display results
