3c443a30ee1bb5fdc7fe37fe6425c57f409261e2
jadensanares <sanaresj@gmail.com>
2025-03-12 08:19:58 +0800

    finished the code by following the coding standard

--- python_practice_batch_2/print_all_oddnumbers_0_to_100_whileloop.py
+++ python_practice_batch_2/print_all_oddnumbers_0_to_100_whileloop.py
@@ -1,4 +1,11 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
 # Initialize a variable to start from 0
+number = 0
+
 # Use a while loop to iterate from 0 to 100
+while number <= 100:
 # Check if the number is odd
+    if number % 2 != 0:
+        print(number) 
+    number += 1
