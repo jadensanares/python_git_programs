504753578a6750d9b686a57ef862cf20bf04589d
jadensanares <sanaresj@gmail.com>
2025-03-12 09:12:21 +0800

    testing if the prompt user is working, and it works

--- python_practice_batch_3/input_10_display_all_duplicated_numbers_display_only_firstentry.py
+++ python_practice_batch_3/input_10_display_all_duplicated_numbers_display_only_firstentry.py
@@ -1,4 +1,12 @@
 # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
-# Initialize the variables
-# Prompt the user input
+
+# Initialize an empty list to store the numbers, Initialize an empty set to keep track of seen numbers
+number_list = []
+seend_numbers = set()
+# Prompt the user input 10 numbers
+for i in range(10):
+    number = int(input(f"Enter a number {i + 1}: ")) # Getting the user input
+    number_list.append(number) # Adding the number to the list
+
 # Display the numbers showing duplicates only once
+print("Numbers (duplicates shonw only once):")
