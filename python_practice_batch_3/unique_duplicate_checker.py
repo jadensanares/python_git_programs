d5145d1fb69a43b515d0608aeeae1b84d234258a
jadensanares <sanaresj@gmail.com>
2025-03-12 09:42:12 +0800

    pseudocode added

---
+++ python_practice_batch_3/unique_duplicate_checker.py
@@ -0,0 +1,5 @@
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+# Initialize emty set to keep track of seen numbers
+# Prompt the user to input a number
+# Check for invalid input (letters)
+# Check if the number has been seen before
