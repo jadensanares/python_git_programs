9ac5215d1cfb3c6545386ec0076d894a91939d5d
jadensanares <sanaresj@gmail.com>
2025-03-12 08:30:31 +0800

    finished the code by following the coding standard

--- python_practice_batch_2/input_2_numbers_print_numbers_between_the_two_numbers.py
+++ python_practice_batch_2/input_2_numbers_print_numbers_between_the_two_numbers.py
@@ -1,4 +1,13 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
 # Ask the user to input 2 numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
 # Finding the smaller and larger number
+start = min(number1, number2)  
+end = max(number1, number2)
+
 # Finding the numbers between the two
+for number in range(start + 1, end):  
+    print(number)
