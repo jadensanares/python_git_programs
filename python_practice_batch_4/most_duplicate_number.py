b713ce0d836eee9d44a2b6c92049113d70a00e0f
jadensanares <sanaresj@gmail.com>
2025-03-12 14:11:12 +0800

    pseudocode for most_duplicate_number.py was added

---
+++ python_practice_batch_4/most_duplicate_number.py
@@ -0,0 +1,7 @@
+# Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+# Make a loop for the continous asking for input
+# Try to convert input to an integer
+# Count how many times the number occured
+# Break loop if conversion fails
+# Find the number with the most duplicates
+# Display the number with the most number of duplicates
