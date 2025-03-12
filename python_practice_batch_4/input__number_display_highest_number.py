f5f7b0df47eb539847d1444b782d9872dbc7b95b
jadensanares <sanaresj@gmail.com>
2025-03-12 17:14:15 +0800

    pseudocode created for Prog03 of batch 4

---
+++ python_practice_batch_4/input__number_display_highest_number.py
@@ -0,0 +1,3 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+# ask the user continously until input is invalid. Display highest number
+
