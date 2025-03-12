8edb24f36fe964b69cda2cac5ed5fa4b731c2701
jadensanares <sanaresj@gmail.com>
2025-03-12 08:25:18 +0800

    finished the code by following the coding standard

--- python_practice_batch_2/print_all_0_to_100_except_ending_in_0_or_5.py
+++ python_practice_batch_2/print_all_0_to_100_except_ending_in_0_or_5.py
@@ -1,3 +1,11 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
 # Initialize a variable to start from 0
+number = 0
+
 # Use a while loop to iterate from 0 to 100 and check if it ends in 0 or 5
+while number <= 100:
+    if number % 10 != 0 and number % 10 != 5:
+        print(number)
+    
+    number += 1
