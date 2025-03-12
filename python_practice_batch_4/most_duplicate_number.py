46fc7bb3315b65ac98a7b201744573ca35bfa285
jadensanares <sanaresj@gmail.com>
2025-03-12 16:59:42 +0800

    testing if the user prompt loop works and removed some line of pseudocode

--- python_practice_batch_4/most_duplicate_number.py
+++ python_practice_batch_4/most_duplicate_number.py
@@ -1,7 +1,18 @@
 # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
 # Make a loop for the continous asking for input
+numbers = []
+while True:
+    user_input = input("Enter a number: ")
+
 # Try to convert input to an integer
-# Count how many times the number occured
-# Break loop if conversion fails
+    try:
+        number = int(user_input)
+        numbers.append(number)
+    except ValueError:
+        # The loop ends if input is invalid and show the result
+        break
+
 # Find the number with the most duplicates
+
 # Display the number with the most number of duplicates
