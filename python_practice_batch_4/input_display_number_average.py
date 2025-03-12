852265473680937f457ed96ec08bc1c47c620e83
jadensanares <sanaresj@gmail.com>
2025-03-12 22:32:16 +0800

    pseudocode added

---
+++ python_practice_batch_4/input_display_number_average.py
@@ -0,0 +1,4 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+# define the variables
+# prompt user for input
+# display the average if the numbers entered were valid
