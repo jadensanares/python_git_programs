ca4d84d731ad3a4d0a259717e2e12b33fb7b7e74
jadensanares <sanaresj@gmail.com>
2025-03-12 10:34:00 +0800

    pseudocode added for Prog05 bath 3

---
+++ python_practice_batch_3/sort_input_numbers.py
@@ -0,0 +1,7 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+# Initialize the empty list to store the numberss
+# make a loop to cintue askign for input
+# prompt the user to enter a number
+# converting input to integer
+# if conversion fails, print the sorted numbers and exit loop
+# exit the loop after showing the results
