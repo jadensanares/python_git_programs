36e7aeca44772d0bed501d63fd28b10fae9c27fe
jadensanares <sanaresj@gmail.com>
2025-03-21 15:49:51 +0800

    Pseudocode added

---
+++ python_practice_batch_5/fullname_reverse_casing.py
@@ -0,0 +1,8 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: JuaN deLA cRuz
+
+# Ask the user to input their full name in incorrect casing
+# Reverss the casing of each character using swapcase
+# Print the result
