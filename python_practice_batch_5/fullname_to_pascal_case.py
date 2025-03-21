0af43a86ea70d8590a8eed2732b8ee3369d8eb45
jadensanares <sanaresj@gmail.com>
2025-03-21 16:19:18 +0800

    Pseudocode added

---
+++ python_practice_batch_5/fullname_to_pascal_case.py
@@ -0,0 +1,8 @@
+# Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+# Example:
+#Input: jUAn DEla CrUZ
+# Output: JuanDelaCruz
+
+# Ask the user to input their full name in incorrect casing
+# Convert the full name into Pascal Case
+# Print the result
