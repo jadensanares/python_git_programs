c1de5f95105da954d49d21df9e020beadab899be
jadensanares <sanaresj@gmail.com>
2025-03-21 16:12:09 +0800

    pseudocode created

---
+++ python_practice_batch_5/character_in_fullname_counter.py
@@ -0,0 +1,8 @@
+# Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+# Example:
+# Input: Juan Dela Cruz
+# Output: 14
+
+# Ask the user to input their full name
+# Make the counter for the characters i full name
+# Print Result
