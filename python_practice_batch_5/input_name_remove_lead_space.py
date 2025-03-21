403a23a2689665c0988e6d8250886f45144bec53
jadensanares <sanaresj@gmail.com>
2025-03-21 14:23:46 +0800

    Making of pseudocode

---
+++ python_practice_batch_5/input_name_remove_lead_space.py
@@ -0,0 +1,5 @@
+#Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+# Asking the user for their full name
+# Removing of leading spaces
+# Print result
