417564cf25b505cf87f4e67cb3d6f58f53427b8e
jadensanares <sanaresj@gmail.com>
2025-03-21 15:08:07 +0800

    pseudocode created for prog3 of batch 5

---
+++ python_practice_batch_5/fullname_converter_to_capital_letter.py
@@ -0,0 +1,7 @@
+# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+# Example:
+# Input: Juan Dela Cruz
+# Output: JUAN DELA CRUZ
+
+# Ask the user to input their fullname
+# Convert the full name into all capital lettters
