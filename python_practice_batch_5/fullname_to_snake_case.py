c7d66c4adba7869c2beac0b705ec9d7d312e58c0
jadensanares <sanaresj@gmail.com>
2025-03-21 16:43:23 +0800

    program finished, name converting to snake case is working

--- python_practice_batch_5/fullname_to_snake_case.py
+++ python_practice_batch_5/fullname_to_snake_case.py
@@ -4,5 +4,10 @@
 # Output: juan_dela_cruz
 
 # Ask the user to input their full name in incorrect casing
+full_name = input("Enter your full name in incorrect casing: ")
+
 # Convert the full name into snake case
+snake_case_name = full_name.lower().replace(" ", "_")
+
 # Print the result
+print("The name in snake case is:", snake_case_name)
