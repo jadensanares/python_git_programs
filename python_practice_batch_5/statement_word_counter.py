fdc5a681880ad34c40249936b180c0cdf841916b
jadensanares <sanaresj@gmail.com>
2025-03-21 16:01:51 +0800

    Pseudocode created

---
+++ python_practice_batch_5/statement_word_counter.py
@@ -0,0 +1,8 @@
+# Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+# Example:
+# Input: We will weather the weather whatever the weather whether we like it or not
+# Output: 14
+
+# Ask the user to input a complete statement
+# Word counter for the statement
+# Print result
