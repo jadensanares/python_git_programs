2d43e1f38148e041e73743e8a15853dbd7fbb5cd
jadensanares <sanaresj@gmail.com>
2025-03-21 14:38:27 +0800

    Pseudocode added for this file

---
+++ python_practice_batch_5/input_number_format_six_digits.py
@@ -0,0 +1,8 @@
+#Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+# Example:
+# Input: 143
+# Output: 000143
+
+# Ask the user to input a number
+# For the number to 6 digits with leading zeroes
+# Print Result
