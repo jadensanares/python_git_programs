132c5a25901c27f36af37e14b1b89743f81a8b09
jadensanares <sanaresj@gmail.com>
2025-03-21 15:55:05 +0800

    testing if the .swap(), program is working

--- python_practice_batch_5/fullname_reverse_casing.py
+++ python_practice_batch_5/fullname_reverse_casing.py
@@ -4,5 +4,10 @@
 # Output: JuaN deLA cRuz
 
 # Ask the user to input their full name in incorrect casing
-# Reverss the casing of each character using swapcase
+full_name = input("Enter your full name in incorrect casing: ")
+
+# Reverse the casing of each character using swapcase
+reversed_casing_name = full_name.swapcase()
+
 # Print the result
+print("Output:", reversed_casing_name)
