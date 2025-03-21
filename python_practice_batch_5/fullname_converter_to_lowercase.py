01f1bfe1c3e5ae8a65bd16691fb602d9d5e2fbc6
jadensanares <sanaresj@gmail.com>
2025-03-21 15:18:58 +0800

    Pseudocode created for the program

---
+++ python_practice_batch_5/fullname_converter_to_lowercase.py
@@ -0,0 +1,8 @@
+# Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+# Example:
+#Input: Juan Dela Cruz
+# Output: juan dela cruz
+
+# Ask the user to input their full name
+# Convert the full name to lowercase format
+# Print the result
