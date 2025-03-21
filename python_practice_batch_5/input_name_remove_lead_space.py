25deb57f36efb06ddbf685ec0e8ecbec48837b0b
jadensanares <sanaresj@gmail.com>
2025-03-21 14:30:15 +0800

    testing of program without using lstrip, results into having spaces

--- python_practice_batch_5/input_name_remove_lead_space.py
+++ python_practice_batch_5/input_name_remove_lead_space.py
@@ -1,5 +1,10 @@
 #Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 # Asking the user for their full name
+full_name = input("Enter your full name: ")
+
 # Removing of leading spaces
+removed_lead_space = full_name
+
 # Print result
+print("Full name: ", removed_lead_space)
